562805617bc23ff5d37326150f1a1b53775f0685
Patrick O'Donnell <podonnell@cloudflare.com>
2026-09-21 12:43:59 -0400

    fixup! WC-6046 [wrangler] previews: normalize D1 metadata bindings

--- packages/deploy-helpers/src/deploy/helpers/provision-bindings.ts
+++ packages/deploy-helpers/src/deploy/helpers/provision-bindings.ts
@@ -653,11 +653,13 @@ class D1Handler extends ProvisionResourceHandler<
 
 			// ...and the user HAS specified a name in their config, so we need to check if the database_name they provided
 			// matches the database_name of the existing binding (which isn't present in settings, so we'll need to make an API call to check).
-			const databaseId =
+			let databaseId: string;
+			if ("database_id" in maybeInherited) {
+				databaseId = maybeInherited.database_id;
+			} else {
 				// oxlint-disable-next-line typescript/no-deprecated -- intentional support of deprecated binding style
-				"database_id" in maybeInherited
-					? maybeInherited.database_id
-					: maybeInherited.id;
+				databaseId = maybeInherited.id;
+			}
 			const dbFromId = await getDatabaseInfoFromIdOrName(
 				this.complianceConfig,
 				this.accountId,
